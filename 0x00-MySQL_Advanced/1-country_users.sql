e5c9284649ad8d75aa06a95817aff4ade25cb364
kumdan job <redeks123456@gmail.com>
2024-01-18 23:00:20 +0100

    advanced sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,3 +1,3 @@
 -- Write a SQL script that creates a table users following these requirements:
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (id INTEGER PRIMARY KEY AUTO_INCREMENT, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country VARCHAR(20) DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN')) NOT NULL)
+CREATE TABLE users (id INTEGER PRIMARY KEY AUTO_INCREMENT, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255), country VARCHAR(20) ENUM ('US', 'CO', 'TN') NOT NULL)
