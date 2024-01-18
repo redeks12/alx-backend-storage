823b6ac8556bb198c2e045f856a710eefc0e2234
kumdan job <redeks123456@gmail.com>
2024-01-18 22:55:41 +0100

    advanced sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,3 @@
 -- Write a SQL script that creates a table users following these requirements:
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) UNIQUE NOT NULL, name VARCHAR(255) NOT NULL);
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) UNIQUE NOT NULL, name VARCHAR(255) NOT NULL);
 
