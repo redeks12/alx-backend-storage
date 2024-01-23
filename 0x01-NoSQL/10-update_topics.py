522aa0eb134ddd930e95cb07589c4a8767ca101d
kumdan job <redeks123456@gmail.com>
2024-01-23 18:40:40 +0100

    m

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
-'''0x01. NoSQL'''
+"""0x01. NoSQL"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Write a Python function that changes all topics of a school document based on the name:"""
+    mongo_collection.update_many({"name": name}, {"topics": topics})
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
 """0x01. NoSQL"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Write a Python function that inserts a new document in a collection based on kwargs"""
+    mongo_collection.insert_one(kwargs)
