8aaca23546c790aafe04415dc54a4ec87736b153
kumdan job <redeks123456@gmail.com>
2024-01-23 19:35:38 +0100

    m

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
-'''0x01. NoSQL'''
+"""0x01. NoSQL"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Write a Python function that returns the list of school having a specific topic:"""
+    val = mongo_collection.find({"topics": topic})
+    return val
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
-'''0x01. NoSQL'''
+"""0x01. NoSQL"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    school_collection = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(school_collection.count_documents({})))
+    print("Methods:")
+    [
+        print(
+            "\tmethod {}: {}".format(
+                val, school_collection.count_documents({"method": val})
+            )
+        )
+        for val in methods
+    ]
+    print(
+        "{} status check".format(school_collection.count_documents({"path": "/status"}))
+    )
