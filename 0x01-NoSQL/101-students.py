4da3088101909dbdfb9e7d260b815290e0ef1c25
kumdan job <redeks123456@gmail.com>
2024-01-23 21:51:13 +0100

    m

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
-'''0x01. NoSQL'''
+"""0x01. NoSQL"""
+
+list_all = __import__("8-all").list_all
+
+
+def top_students(mongo_collection):
+    """Write a Python function that returns all students sorted by average score"""
+    arrs = mongo_collection.find()
+    news = []
+    for i in arrs:
+        bb = 0
+        for a in i.get("topics"):
+            bb += a.get("score")
+
+        i["averageScore"] = bb / 3
+        news.append(i)
+    return sorted(news, key=lambda x: x["averageScore"], reverse=True)
