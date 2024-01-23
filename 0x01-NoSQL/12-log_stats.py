f2aa8b7f432ab5090f9d2c6f0001ea5fa35b5856
kumdan job <redeks123456@gmail.com>
2024-01-23 21:10:40 +0100

    m

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
-"""Python script that provides stats about Nginx logs stored in MongoDB"""
-import pymongo
+"""0x01. NoSQL"""
+from pymongo import MongoClient
 
 if __name__ == "__main__":
-    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
-    collection = client.logs.nginx
-
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    print("Methods:")
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    db = client.logs.nginx
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    status_check_count = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-
-    print(f"{status_check_count} status check")
+    print(f"{db.count_documents({})} logs")
+    print("Methods:")
+    [print(f"\tmethod {val}: {db.count_documents({'method': val})}") for val in methods]
+    print(f"{db.count_documents({'method': 'GET', 'path': '/status'})} status check")
