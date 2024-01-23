af4a5908e13526fe664da6c57cae262c60a60ed0
kumdan job <redeks123456@gmail.com>
2024-01-23 19:03:59 +0100

    m

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,4 +5,4 @@
 def insert_school(mongo_collection, **kwargs):
     """Write a Python function that inserts a new document in a collection based on kwargs"""
     result = mongo_collection.insert_one(kwargs)
-    return result._id
+    return result.inserted_id
