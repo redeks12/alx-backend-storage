9d12959a02481889a110f0c2f40771aaa5c2ff77
kumdan job <redeks123456@gmail.com>
2024-01-23 19:46:59 +0100

    m

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,27 +1,12 @@
 #!/usr/bin/env python3
-"""Log stats module"""
+"""0x01. NoSQL"""
 from pymongo import MongoClient
-
 
 if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     db = client.logs.nginx
-
-    num_logs = db.count_documents({})
-    print(f"{num_logs} logs")
-
-    get = db.count_documents({"method": "GET"})
-    post = db.count_documents({"method": "POST"})
-    put = db.count_documents({"method": "PUT"})
-    patch = db.count_documents({"method": "PATCH"})
-    delete = db.count_documents({"method": "DELETE"})
-
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{db.count_documents({})} logs")
     print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-
-    status = db.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status} status check")
+    [print(f"\tmethod {val}: {db.count_documents({'method': val})}") for val in methods]
+    print(f"{db.count_documents({'method': 'GET', 'path': '/status'})} status check")
