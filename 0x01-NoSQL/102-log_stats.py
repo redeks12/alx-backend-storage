df9ffcbec625702e3b1e0d8234c7c7c348f53324
kumdan job <redeks123456@gmail.com>
2024-01-23 22:28:06 +0100

    m

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
-'''0x01. NoSQL'''
+"""0x01. NoSQL"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    db = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f"{db.count_documents({})} logs")
+    print("Methods:")
+    [print(f"\tmethod {val}: {db.count_documents({'method': val})}") for val in methods]
+    print(f"{db.count_documents({'method': 'GET', 'path': '/status'})} status check")
+    schema = [
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {"_id": 0, "ip": "$_id", "count": 1}},
+    ]
+    [print("\t{}: {}".format(i["ip"], i["count"])) for i in db.aggregate(schema)]
