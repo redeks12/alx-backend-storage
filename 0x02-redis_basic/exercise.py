eddae5f50a1404f2e9a5e03bd6656a4bf81928b1
kumdan job <redeks123456@gmail.com>
2024-01-24 16:01:18 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,11 +8,15 @@ import redis
 
 
 class Cache:
+    """Cache implementation"""
+
     def __init__(self) -> None:
+        """initialize the cache  object"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, int, bytes, float]) -> str:
+        """Writing strings to Redis"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
